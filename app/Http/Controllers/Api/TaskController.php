6949779b388dff2fbb0b5eca4cbcde0e2184d02d
Lameck Meshack <rhinolamer@gmail.com>
2023-04-18 14:53:16 +0300

    Add controller and resource

---
+++ app/Http/Controllers/Api/TaskController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Api\BaseController as BaseController;
+use App\Models\Task;
+use Illuminate\Support\Facades\Validator;
+use App\Http\Resources\TaskResource;
+
+class TaskController extends BaseController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $tasks = Task::all();
+
+        return $this->sendResponse(TaskResource::collection($tasks), 'Tasks retrieved successfully.');
+    }
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $input = $request->all();
+
+        $validator = Validator::make($input, [
+            'name' => 'required',
+            'description' => 'required',
+            'due_date' => 'required',
+            'status_id' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return $this->sendError('Validation Error.', $validator->errors());
+        }
+
+        $task = Task::create($input);
+
+        return $this->sendResponse(new TaskResource($task), 'Task created successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $task = Task::find($id);
+
+        if (is_null($task)) {
+            return $this->sendError('Task not found.');
+        }
+
+        return $this->sendResponse(new TaskResource($task), 'Task retrieved successfully.');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Task $task)
+    {
+        $input = $request->all();
+
+        $validator = Validator::make($input, [
+            'name' => 'required',
+            'description' => 'required',
+            'due_date' => 'required',
+            'status_id' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return $this->sendError('Validation Error.', $validator->errors());
+        }
+
+        $task->name = $input['name'];
+        $task->description = $input['description'];
+        $task->due_date = $input['due_date'];
+        $task->status_id = $input['status_id'];
+        $task->save();
+
+        return $this->sendResponse(new TaskResource($task), 'Task updated successfully.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Task $task)
+    {
+        $task->delete();
+
+        return $this->sendResponse([], 'Task deleted successfully.');
+    }
+}
