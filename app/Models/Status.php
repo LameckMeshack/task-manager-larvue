cf3c192962321afa1bfa722e9e2d9c3f52dd28f5
Lameck Meshack <rhinolamer@gmail.com>
2023-04-18 14:53:50 +0300

    Update status table and migration to set defaults

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -12,4 +12,20 @@ class Status extends Model
     protected $fillable = [
         'name',
     ];
+
+    protected $table = 'status';
+
+    // Register a saving event that will be called before saving the model
+    // protected static function boot()
+    // {
+    //     parent::boot();
+
+    //     static::saving(function ($status) {
+    //         // Validate the name field to only allow the values 'Not Started', 'In Progress' and 'Completed'
+    //         $allowedNames = ['Not Started', 'In Progress', 'Completed'];
+    //         if (!in_array($status->name, $allowedNames)) {
+    //             throw new \Exception('Invalid status name. Allowed values are: ' . implode(', ', $allowedNames));
+    //         }
+    //     });
+    // }
 }
--- database/migrations/2023_04_18_081907_create_status_table.php
+++ database/migrations/2023_04_18_081907_create_status_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\Status;
 
 return new class extends Migration
 {
@@ -17,6 +18,12 @@ return new class extends Migration
             $table->timestamps();
             $table->softDeletes();
         });
+
+        // Insert the required records into the status table
+        $statusValues = ['Not Started', 'In Progress', 'Completed'];
+        foreach ($statusValues as $value) {
+            Status::create(['name' => $value]);
+        }
     }
 
     /**
@@ -24,6 +31,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('statuses');
+        Schema::dropIfExists('status');
     }
 };
